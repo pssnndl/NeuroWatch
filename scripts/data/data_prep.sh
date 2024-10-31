1b20ef3b906fbeceb017229d001a91a5a9ce7379
prerana <prerana.chaks@gmail.com>
2024-10-31 11:50:56 +0800

    function modified to handle multiple arguments

--- scripts/data/data_prep.sh
+++ scripts/data/data_prep.sh
@@ -1,2 +1,2 @@
-export PATH_TO_EEG="../../datasets/raw_data/v2.0.1/"
-python preprocess_data.py --data_type train --cpu_num 4 --label_type  "tse" --save_directory "../../datasets/processed_data"
+export PATH_TO_EEG="../../datasets/raw_data/v2.0.3/edf/"
+python preprocess_data.py --data_type train --cpu_num 4 --label_type  "csv" --save_directory "../../datasets/processed_data/"
--- scripts/data/utils/process_util.py
+++ scripts/data/utils/process_util.py
@@ -3,14 +3,15 @@
 from multiprocessing import Pool
 from tqdm import tqdm
 
-
-def run_multi_process(f, l: list, n_processes=40):
+# Modify the call in run_multi_process
+def run_multi_process(f, l: list, second_arg, n_processes=40):
     n_processes = min(n_processes, len(l))
     print(n_processes)
 
-    results = list()
+    results = []
     pool = Pool(processes=n_processes)
-    for r in tqdm(pool.imap_unordered(f, l), total=len(l), ncols=75):
+    func_args = [(item, second_arg) for item in l]  # Pair each element with the second_arg
+    for r in tqdm(pool.imap_unordered(f, func_args), total=len(l), ncols=75):
         results.append(r)
 
     pool.close()
